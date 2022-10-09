acbe97f5fe8435cf96dd62413c482fe5bca629c0
nxhdev2002 <xuanhoangv7c@gmail.com>
2022-10-09 23:49:01 +0700

    update nav toggle

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.getElementById("nav-toggle").addEventListener("click", () => {
+        let action;
+        document.getElementById("side-nav-container").getAttribute("class") === "expanded" ? action = "collapsed" : action = "expanded"
+        document.getElementById("side-nav-container").setAttribute("class", action)
+    })
+});
